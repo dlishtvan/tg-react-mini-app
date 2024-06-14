43040dd98e11ad2e2efdc6720ae58a15ac22c190
Dmytro <dmitriylishtvan@gmail.com>
2024-06-14 23:10:15 +0300

    build failed, eslint errors

--- src/layouts/Empty.js
+++ src/layouts/Empty.js
@@ -1,23 +1,12 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Outlet} from 'react-router';
-import {useTelegram} from '../hooks/useTelegram';
-// import {Header} from "../components/Header";
-// import {Footer} from "../components/Footer";
 
 export const Empty = () => {
-  console.log('ROOT');
-  const {tg} = useTelegram();
-
-  useEffect(() => {
-    tg.ready();
-  }, []);
-
-
   return (
     <>
       <main>
         <div id="container" className="container py-3">
-                   EMPTY
+          EMPTY
           <Outlet/>
         </div>
       </main>
