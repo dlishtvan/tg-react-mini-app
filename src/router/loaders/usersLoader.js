941b4bc7d44e23deb4b138985014530cfabb8a50
Dmytro <dmitriylishtvan@gmail.com>
2024-06-27 14:55:18 +0300

    files naming, footer UI, fix typo, remove unneeded dir (mineLoader)

--- src/router/loaders/usersLoader.js
+++ src/router/loaders/usersLoader.js
@@ -3,8 +3,13 @@ import {fetchUsers} from '../../features/user/userAPI';
 
 const userLoader = async () => {
   const {dispatch} = store;
+  const {user} = store.getState();
 
   try {
+    if (user.list.length) {
+      return null;
+    }
+
     await dispatch(fetchUsers());
   } catch (err) {
     console.error('Error fetching or creating user:', err);
